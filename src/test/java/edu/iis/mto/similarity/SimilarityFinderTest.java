8e4e4a3a1e92f1a3af57499e880503dede6ff9e1
kateryna <katerynatsarova@gmail.com>
2021-03-22 20:08:15 +0200

    adding tests for result of calculateJackardSimilarity

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -2,13 +2,83 @@ package edu.iis.mto.similarity;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
+import edu.iis.mto.searcher.SearchResult;
+import edu.iis.mto.searcher.SequenceSearcher;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class SimilarityFinderTest {
+import java.util.Arrays;
 
+class SimilarityFinderTest {
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void similarityFinder_seq1Empty_seq2Empty(){
+        int[] seq1 = {};
+        int[] seq2 = {};
+
+        SequenceSearcher seqserch = new SequenceSearcher() {
+            @Override
+            public SearchResult search(int elem, int[] sequence) {
+                for(int i = 0; i<sequence.length; i++){
+                    if(sequence[i] == elem){
+                        SearchResult sr = SearchResult.Builder.withFound(true).withPosition(i).build();
+                        return sr;
+                    }
+                }
+                SearchResult sr = SearchResult.Builder.withFound(false).withPosition(-1).build();
+                return sr;
+            }
+        };
+
+        SimilarityFinder sf = new SimilarityFinder(seqserch);
+
+        Assertions.assertTrue(sf.calculateJackardSimilarity(seq1, seq2)==1);
     }
 
+    @Test
+    void similarityFinder_seq1Empty_seq2NotEmpty(){
+        int[] seq1 = {};
+        int[] seq2 = {1, 2};
+
+        SequenceSearcher seqserch = new SequenceSearcher() {
+            @Override
+            public SearchResult search(int elem, int[] sequence) {
+                for(int i = 0; i<sequence.length; i++){
+                    if(sequence[i] == elem){
+                        SearchResult sr = SearchResult.Builder.withFound(true).withPosition(i).build();
+                        return sr;
+                    }
+                }
+                SearchResult sr = SearchResult.Builder.withFound(false).withPosition(-1).build();
+                return sr;
+            }
+        };
+
+        SimilarityFinder sf = new SimilarityFinder(seqserch);
+
+        Assertions.assertTrue(sf.calculateJackardSimilarity(seq1, seq2)==0);
+    }
+
+    @Test
+    void similarityFinder_seq1NotEmpty_seq2Empty(){
+        int[] seq1 = {1, 2};
+        int[] seq2 = {};
+
+        SequenceSearcher seqserch = new SequenceSearcher() {
+            @Override
+            public SearchResult search(int elem, int[] sequence) {
+                for(int i = 0; i<sequence.length; i++){
+                    if(sequence[i] == elem){
+                        SearchResult sr = SearchResult.Builder.withFound(true).withPosition(i).build();
+                        return sr;
+                    }
+                }
+                SearchResult sr = SearchResult.Builder.withFound(false).withPosition(-1).build();
+                return sr;
+            }
+        };
+
+        SimilarityFinder sf = new SimilarityFinder(seqserch);
+
+        Assertions.assertTrue(sf.calculateJackardSimilarity(seq1, seq2)==0);
+    }
 }
