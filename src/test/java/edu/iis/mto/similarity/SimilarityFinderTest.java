63c8bab5a17a2a99197a46c45662ed7617b5e217
kateryna <katerynatsarova@gmail.com>
2021-03-23 00:37:33 +0200

    adding tests of function behavior

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
 
 class SimilarityFinderTest {
     @Test
-    void similarityFinder_seq1Empty_seq2Empty(){
+    void seq1Empty_seq2Empty_equalArrays(){
         int[] seq1 = {};
         int[] seq2 = {};
 
@@ -35,7 +35,7 @@ class SimilarityFinderTest {
     }
 
     @Test
-    void similarityFinder_seq1Empty_seq2NotEmpty(){
+    void seq1Empty_seq2NotEmpty_notEqualArrays(){
         int[] seq1 = {};
         int[] seq2 = {1, 2};
 
@@ -59,7 +59,7 @@ class SimilarityFinderTest {
     }
 
     @Test
-    void similarityFinder_seq1NotEmpty_seq2Empty(){
+    void seq1NotEmpty_seq2Empty_notEqualArrays(){
         int[] seq1 = {1, 2};
         int[] seq2 = {};
 
@@ -83,7 +83,7 @@ class SimilarityFinderTest {
     }
 
     @Test
-    void similarityFinder_seq1NotEmpty_seq2NotEmpty_equalArrays(){
+    void seq1NotEmpty_seq2NotEmpty_equalArrays(){
         int[] seq1 = {1, 2};
         int[] seq2 = {1, 2};
 
@@ -107,7 +107,7 @@ class SimilarityFinderTest {
     }
 
     @Test
-    void similarityFinder_seq1NotEmpty_seq2NotEmpty_notEqualArrays_arraysHaveSameElem(){
+    void seq1NotEmpty_seq2NotEmpty_notEqualArrays_arraysHaveSameElem(){
         int[] seq1 = {1, 2};
         int[] seq2 = {1, 2, 3, 4};
 
@@ -131,7 +131,7 @@ class SimilarityFinderTest {
     }
 
     @Test
-    void similarityFinder_seq1NotEmpty_seq2NotEmpty_notEqualArrays_arraysDontHaveSameElem(){
+    void seq1NotEmpty_seq2NotEmpty_notEqualArrays_arraysDontHaveSameElem(){
         int[] seq1 = {5, 6, 7, 8};
         int[] seq2 = {1, 2, 3, 4};
 
@@ -152,5 +152,86 @@ class SimilarityFinderTest {
         SimilarityFinder sf = new SimilarityFinder(seqserch);
 
         Assertions.assertTrue(sf.calculateJackardSimilarity(seq1, seq2)==0);
+    }
+
+    @Test
+    public void countingSearchingInvokes_resEqualsLenOfFirstSeq_resShouldBeZero() throws NoSuchFieldException, IllegalAccessException {
+        int[] seq1 = {};
+        int[] seq2 = {};
+
+        SequenceSearcher seqserch = new SequenceSearcher() {
+            public int count = 0;
+            @Override
+            public SearchResult search(int elem, int[] sequence) {
+                count++;
+                for(int i = 0; i<sequence.length; i++){
+                    if(sequence[i] == elem){
+                        SearchResult sr = SearchResult.builder().withFound(true).withPosition(i).build();
+                        return sr;
+                    }
+                }
+                SearchResult sr = SearchResult.builder().withFound(false).withPosition(-1).build();
+                return sr;
+            }
+        };
+
+        SimilarityFinder sf = new SimilarityFinder(seqserch);
+        sf.calculateJackardSimilarity(seq1, seq2);
+
+        Assertions.assertTrue(seqserch.getClass().getDeclaredField("count").getInt(seqserch) == 0);
+    }
+
+    @Test
+    public void countingSearchingInvokes_resEqualsLenOfFirstSeq_resShouldBeOne() throws NoSuchFieldException, IllegalAccessException {
+        int[] seq1 = {1};
+        int[] seq2 = {1, 2, 3, 4};
+
+        SequenceSearcher seqserch = new SequenceSearcher() {
+            public int count = 0;
+            @Override
+            public SearchResult search(int elem, int[] sequence) {
+                count++;
+                for(int i = 0; i<sequence.length; i++){
+                    if(sequence[i] == elem){
+                        SearchResult sr = SearchResult.builder().withFound(true).withPosition(i).build();
+                        return sr;
+                    }
+                }
+                SearchResult sr = SearchResult.builder().withFound(false).withPosition(-1).build();
+                return sr;
+            }
+        };
+
+        SimilarityFinder sf = new SimilarityFinder(seqserch);
+        sf.calculateJackardSimilarity(seq1, seq2);
+
+        Assertions.assertTrue(seqserch.getClass().getDeclaredField("count").getInt(seqserch) == 1);
+    }
+
+    @Test
+    public void countingSearchingInvokes_resEqualsLenOfFirstSeq_resShouldBeTwo() throws NoSuchFieldException, IllegalAccessException {
+        int[] seq1 = {1, 6};
+        int[] seq2 = {1, 2, 3, 4};
+
+        SequenceSearcher seqserch = new SequenceSearcher() {
+            public int count = 0;
+            @Override
+            public SearchResult search(int elem, int[] sequence) {
+                count++;
+                for(int i = 0; i<sequence.length; i++){
+                    if(sequence[i] == elem){
+                        SearchResult sr = SearchResult.builder().withFound(true).withPosition(i).build();
+                        return sr;
+                    }
+                }
+                SearchResult sr = SearchResult.builder().withFound(false).withPosition(-1).build();
+                return sr;
+            }
+        };
+
+        SimilarityFinder sf = new SimilarityFinder(seqserch);
+        sf.calculateJackardSimilarity(seq1, seq2);
+
+        Assertions.assertTrue(seqserch.getClass().getDeclaredField("count").getInt(seqserch) == 2);
     }
 }
